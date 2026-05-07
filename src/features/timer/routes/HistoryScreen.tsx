3da32d226ee0750cbc0ad520fb8e22e1aa002a4c
Bruno <alanbrunoriosmiguel@gmail.com>
2026-05-07 18:07:40 -0300

    feat: add components page domain timer

---
+++ src/features/timer/routes/HistoryScreen.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from 'react';
+import { useTaskContext } from '../store';
+import type { SortTasksOptions } from '../types';
+import { sortTasks } from '../util/sortTasks';
+import { Alert } from '@/lib/alert';
+import { Heading } from '@/components/ui/Heading';
+import { TableTimer } from '../components/TableTimer';
+import { ContainerButton } from '../components/ContainerButton';
+import { useTranslation } from 'react-i18next';
+
+export function HistoryScreen() {
+  const { state, dispatch } = useTaskContext();
+  const [confirmClearHistory, setConfirmClearHistory] = useState(false);
+  const hasTasks = state.tasks.length > 0;
+  const { t } = useTranslation('timer');
+  const [sortTasksOptions, setSortTaskOptions] = useState<SortTasksOptions>(
+    () => {
+      return {
+        tasks: sortTasks({ tasks: state.tasks }),
+        field: 'startDate',
+        direction: 'desc',
+      };
+    },
+  );
+
+  useEffect(() => {
+    setSortTaskOptions(prevState => ({
+      ...prevState,
+      tasks: sortTasks({
+        tasks: state.tasks,
+        direction: prevState.direction,
+        field: prevState.field,
+      }),
+    }));
+  }, [state.tasks]);
+
+  useEffect(() => {
+    document.title = t('timer.history') + ' - Aion Pomodoro';
+  }, []);
+
+  useEffect(() => {
+    if (!confirmClearHistory) return;
+
+    setConfirmClearHistory(false);
+
+    dispatch({ type: 'RESET_STATE' });
+  }, [confirmClearHistory, dispatch]);
+
+  useEffect(() => {
+    return () => {
+      Alert.dismiss();
+    };
+  }, []);
+
+  function handleSortTasks({ field }: Pick<SortTasksOptions, 'field'>) {
+    const newDirection = sortTasksOptions.direction === 'desc' ? 'asc' : 'desc';
+
+    setSortTaskOptions({
+      tasks: sortTasks({
+        direction: newDirection,
+        tasks: sortTasksOptions.tasks,
+        field,
+      }),
+      direction: newDirection,
+      field,
+    });
+  }
+
+  function handleResetHistory() {
+    Alert.dismiss();
+    Alert.confirm(t('timer.heSure'), confirmation => {
+      setConfirmClearHistory(confirmation);
+    });
+  }
+
+  return (
+    <>
+      <Heading>
+        <span>{t('timer.history')}</span>
+        {hasTasks && <ContainerButton onClick={handleResetHistory} />}
+      </Heading>
+
+      {hasTasks && (
+        <TableTimer
+          tasks={sortTasksOptions.tasks}
+          activeTask={state.activeTask}
+          handleSortTasks={handleSortTasks}
+        />
+      )}
+
+      {!hasTasks && (
+        <p style={{ textAlign: 'center', fontWeight: 'bold' }}>
+          {t('timer.notTaskCreate')}
+        </p>
+      )}
+    </>
+  );
+}
---
+++ src/features/timer/routes/SettingsScreen.tsx
@@ -0,0 +1,110 @@
+import { Alert } from '@/lib/alert';
+import { useTaskContext } from '../store';
+import { useEffect, useRef } from 'react';
+import { Heading } from '@/components/ui/Heading';
+import { Input } from '@/components/ui/Input';
+import { Button } from '@/components/ui/Button';
+import { SaveIcon } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
+
+export function SettingsScreen() {
+  const { state, dispatch } = useTaskContext();
+  const workTimeInput = useRef<HTMLInputElement>(null);
+  const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+  const longBreakTimeInput = useRef<HTMLInputElement>(null);
+  const { t } = useTranslation('timer');
+
+  useEffect(() => {
+    document.title = t('timer.settings') + ' - Aion Pomodoro';
+  }, []);
+
+  function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    Alert.dismiss();
+
+    const formErrors = [];
+
+    const workTime = Number(workTimeInput.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+    const longBreakTime = Number(longBreakTimeInput.current?.value);
+
+    if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+      formErrors.push(t('timer.enterNumbersAllFields'));
+    }
+
+    if (workTime < 1 || workTime > 99) {
+      formErrors.push(t('timer.enterBetween1And99'));
+    }
+
+    if (shortBreakTime < 1 || shortBreakTime > 30) {
+      formErrors.push(t('timer.enterBetween1And30'));
+    }
+
+    if (longBreakTime < 1 || longBreakTime > 60) {
+      formErrors.push(t('timer.enterBetween1And60'));
+    }
+
+    if (formErrors.length > 0) {
+      formErrors.forEach(error => {
+        Alert.error(error);
+      });
+      return;
+    }
+
+    dispatch({
+      type: 'CHANGE_SETTINGS',
+      payload: {
+        workTime,
+        shortBreakTime,
+        longBreakTime,
+      },
+    });
+    Alert.success(t('timer.settingsSave'));
+  }
+
+  return (
+    <>
+      <Heading>{t('timer.settings')}</Heading>
+
+      <p style={{ textAlign: 'center' }}>{t('timer.changeSettingTimeFocus')}</p>
+
+      <form onSubmit={handleSaveSettings} action='' className='form'>
+        <div className='formRow'>
+          <Input
+            id='workTime'
+            labelText={t('timer.workTime')}
+            ref={workTimeInput}
+            defaultValue={state.config.workTime}
+            type='number'
+          />
+        </div>
+        <div className='formRow'>
+          <Input
+            id='shortBreakTime'
+            labelText={t('timer.shortBreakTime')}
+            ref={shortBreakTimeInput}
+            defaultValue={state.config.shortBreakTime}
+            type='number'
+          />
+        </div>
+        <div className='formRow'>
+          <Input
+            id='longBreakTime'
+            labelText={t('timer.longBreakTime')}
+            ref={longBreakTimeInput}
+            defaultValue={state.config.longBreakTime}
+            type='number'
+          />
+        </div>
+        <div className='formRow'>
+          <Button
+            aria-label={t('timer.saveSettings')}
+            title={t('timer.saveSettings')}
+          >
+            <SaveIcon />
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+}
