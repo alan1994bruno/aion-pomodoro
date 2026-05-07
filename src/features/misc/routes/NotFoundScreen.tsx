a64b03373dad403958e23c08e6750153d30cbb6e
Bruno <alanbrunoriosmiguel@gmail.com>
2026-05-07 17:53:39 -0300

    feat: add components domain misc

---
+++ src/features/misc/routes/NotFoundScreen.tsx
@@ -0,0 +1,27 @@
+import { Heading } from '@/components/ui/Heading';
+import { Link } from '@/components/ui/Link';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export function NotFoundScreen() {
+  const { t } = useTranslation('misc');
+  useEffect(() => {
+    document.title = t('misc.pageNotFound') + ' - Aion Pomodoro';
+  }, []);
+
+  return (
+    <>
+      <Heading>404 - {t('misc.pageNotFound')} 🚀</Heading>
+      <p>{t('misc.opsNotFoutnd')} 🌌</p>
+      <p>
+        {t('misc.butWorrySafely')} <Link href='/'>{t('misc.homePage')}</Link>{' '}
+        {t('misc.or')} <Link href='/history/'>{t('misc.theHistory')}</Link> —{' '}
+        {t('misc.youCanPretend')} 🧭✨
+      </p>
+      <p>{t('misc.pageShouldExist')}</p>
+      <p>
+        {t('misc.somethingAbout')} "{t('misc.existInternet')}" 🤔💭
+      </p>
+    </>
+  );
+}
