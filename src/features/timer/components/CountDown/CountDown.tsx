07efd70ad05fae8672cfc016f27ba042eda3ce11
Bruno <alanbrunoriosmiguel@gmail.com>
2026-05-07 18:07:11 -0300

    feat: add components domain timer

---
+++ src/features/timer/components/CountDown/CountDown.tsx
@@ -0,0 +1,10 @@
+import { useTaskContext } from '../../store';
+import styles from './CountDown.module.css';
+
+export function CountDown() {
+  const { state } = useTaskContext();
+
+  return (
+    <div className={styles.container}>{state.formattedSecondsRemaining}</div>
+  );
+}
