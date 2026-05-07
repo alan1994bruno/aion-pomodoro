365d69e0066f1d1baf1e3940b8f3e9033bd46098
Bruno <alanbrunoriosmiguel@gmail.com>
2026-05-07 18:16:20 -0300

    feat: add codes in main  files

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/globals.css';
 import App from './App.tsx';
+import { TaskContextProvider } from './features/timer/store/TaskContextProvider.tsx';
+import { MessagesContainer } from './components/layout/MessagesContainer/MessageContainer.tsx';
+import './lib/i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TaskContextProvider>
+      <MessagesContainer>
+        <App />
+      </MessagesContainer>
+    </TaskContextProvider>
   </StrictMode>,
 );
