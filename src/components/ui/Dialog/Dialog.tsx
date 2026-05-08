173d2e5f098c415240f774018c1a4ff25c5d87a8
Bruno <alanbrunoriosmiguel@gmail.com>
2026-05-08 09:37:14 -0300

    fix: solve problem message dialog

--- src/components/ui/Dialog/Dialog.tsx
+++ src/components/ui/Dialog/Dialog.tsx
@@ -1,13 +1,10 @@
 import type { ToastContentProps } from 'react-toastify';
-
 import { ThumbsDownIcon, ThumbsUpIcon } from 'lucide-react';
-import { useTranslation } from 'react-i18next';
-
 import styles from './Dialog.module.css';
 import { Button } from '../Button';
+import i18n from '@/lib/i18n';
 
 export function Dialog({ closeToast, data }: ToastContentProps<string>) {
-  const { t } = useTranslation();
   return (
     <div className={styles.container}>
       <p>{data}</p>
@@ -15,16 +12,16 @@ export function Dialog({ closeToast, data }: ToastContentProps<string>) {
       <div className={styles.buttonsContainer}>
         <Button
           onClick={() => closeToast(true)}
-          aria-label={t('commom.confirmAction')}
-          title={t('commom.confirmAction')}
+          aria-label={i18n.t('commom.confirmAction')}
+          title={i18n.t('commom.confirmAction')}
         >
           <ThumbsUpIcon />
         </Button>
         <Button
           onClick={() => closeToast(false)}
           color='red'
-          aria-label={t('commom.cancelAction')}
-          title={t('commom.cancelAction')}
+          aria-label={i18n.t('commom.cancelAction')}
+          title={i18n.t('commom.cancelAction')}
         >
           <ThumbsDownIcon />
         </Button>
